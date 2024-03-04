ef75ffe31002db196c89a7b4910e8fef3ff03e61
furahapa <149945815+furahapa@users.noreply.github.com>
2024-03-04 20:43:19 +0000

    Cereated files

---
+++ index.js
@@ -0,0 +1,29 @@
+class DataHolder {
+    constructor() {
+      this.data = {};
+    }
+  
+    setData(key, value) {
+      this.data[key] = value;
+      console.log(`Set data for key '${key}':`, value);
+    }
+  
+    getData(key) {
+      const value = this.data[key];
+      console.log(`Retrieved data for key '${key}':`, value);
+      return value;
+    }
+  }
+  
+  // Example usage:
+  const dataHolder = new DataHolder();
+  
+  dataHolder.setData('name', 'John Doe');
+  dataHolder.setData('age', 25);
+  
+  const retrievedName = dataHolder.getData('name');
+  const retrievedAge = dataHolder.getData('age');
+  const retrievedUnknown = dataHolder.getData('unknownKey');
+  
+  console.log('Retrieved data for unknownKey:', retrievedUnknown);
+  
